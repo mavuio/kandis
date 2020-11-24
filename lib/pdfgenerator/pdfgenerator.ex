77d9ddd8574759c4557b105987655350626527cf
Manfred Wuits <manfred@monochrom.at>
2020-11-24 15:28:42 +0100

    retro-fixed 0.3.4 for evablut

--- lib/pdfgenerator/pdfgenerator.ex
+++ lib/pdfgenerator/pdfgenerator.ex
@@ -18,7 +18,7 @@ defmodule Kandis.Pdfgenerator do
         if generated_filename == filename do
           filename
         else
-          raise "filenames do not match #{generated_filename} vs #{filename}"
+          nil
         end
     end
   end
